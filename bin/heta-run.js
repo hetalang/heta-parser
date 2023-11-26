84095235ed1667f5359ac3bc4848a057c91e215b
Evgeny Metelkin <metelkin@insysbio.com>
2023-11-26 10:18:03 +0200

    update deps

--- bin/heta-run.js
+++ bin/heta-run.js
@@ -9,23 +9,26 @@ commander
   .description('Run heta files, return JavaScript object')
   .usage('[inputFile]')
   .option('-o, --output <path>', 'save result to json file')
-  .action((input, cmd) => {
-    fs.readFile(input, 'utf8', (err, contents) => {
-      if (input.match(/.heta$/) === null) throw new Error('Incorrect file type');
-
-      if (err) {
-        process.stderr.write(err.message);
-      } else {
-        let result = parse(contents);
-        if (cmd.output) {
-          fs.writeFile(cmd.output, JSON.stringify(result, null, 2), (err) => {
-            if (err) throw err;
-            process.stdout.write(`Result successfully written to file: ${cmd.output}.`);
-          });
-        } else {
-          process.stdout.write(JSON.stringify(result, null, 2));
-        }
-      }
-    });
-  })
   .parse(process.argv);
+
+
+let input = commander.args[0];
+let opts = commander.opts();
+
+fs.readFile(input, 'utf8', (err, contents) => {
+  if (input.match(/.heta$/) === null) throw new Error('Incorrect file type');
+
+  if (err) {
+    process.stderr.write(err.message);
+  } else {
+    let result = parse(contents);
+    if (opts.output) {
+      fs.writeFile(opts.output, JSON.stringify(result, null, 2), (err) => {
+        if (err) throw err;
+        process.stdout.write(`Result successfully written to file: ${opts.output}.`);
+      });
+    } else {
+      process.stdout.write(JSON.stringify(result, null, 2));
+    }
+  }
+});
